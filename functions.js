62a97b7187ad84fc0e478c47becad9bfd9902002
Kyle Grinstead <kyle@kylegrinstead.com>
2013-07-08 22:17:33 -0400

    Completely and utterly refactored the funtions.js file. It is now much cleaner and doesn't produce any errors, simply because only the clickable cards are bound. This means that no mistakes can be made unless the user tries to cheat in the console. And that of course is handled in the backend.

--- functions.js
+++ functions.js
@@ -21,14 +21,15 @@ if (typeof playerNameTurn === 'undefined') {
 	var playerNameTurn = null;
 }
 
-var $compareCard;
+var compareCard;
 var playerNumber;
 var cardNumber;
 
 
-function gameAlert(alertMessage) {
+function gameAlert(alertMessage, color) {
 	$('.game_alerts').html('');
 	$('.game_alerts').append(alertMessage);
+	$('.game_alerts').addClass(color);
 }
 
 function gameAction(action, playerNum, cardNum) {
@@ -47,124 +48,121 @@ function gameAction(action, playerNum, cardNum) {
 		});
 	}
 	else {
-		resetBinds();
+		setClickable('reset');
 		$('.game_alerts').addClass('red');
 		return 'Sorry but it is '+playerNameTurn+"'s turn. Wait for your turn.";
 	}
 }
 
-function resetBinds() {
-	$('body').off('click', '.card') ;
-	$('body').on('click', '.card', function() {
-		cardSwitch($(this));
-	});
-}
-
 function fadeError() {
-	$('.game_alerts').fadeTo('slow', 0.1, function() {
+	$('.game_alerts').fadeTo('slow', 0.01, function() {
 		$(this).removeClass('red');
 		$(this).css('opacity', '1');
 		gameAlert('');
 	});
 }
 
-function resetClickableClass() {
-	$('html').find('.clickable').removeClass('.clickable');
+function cardIsClicked(card) {
+	noCardsClicked();
+	card.addClass('clicked');
 }
 
-function setClickableClass(status) {
-	if (typeof status === 'undefined') {
-		resetClickableClass();
-		$('.player').eq(turn).find('.card.hidden').addClass('clickable');
-		$('.draw_pile').children().children().addClass('clickable');
-		$('.discard_pile').children().children().addClass('clickable');
-	}
+function noCardsClicked() {
+	$('.card').removeClass('clicked');
+}
+
+function setClickable(status) {
+	$('html').find('.clickable').removeClass('.clickable');
+	
+	var $playerDrawDiscard = $('.card').not('.player .card').add('.player:eq('+turn+') .card');
+	var $clickableCards = $('.card').off('click');
+
 	switch (status) {
-		case 'userCardsEnabled':
-			$('.player').eq(turn).find('.card').addClass('clickable');
+		case 'reset':
+			$clickableCards = $playerDrawDiscard.not('.player .showing').addClass('clickable');
+			$clickableCards.click(function() {
+				cardSwitch($(this));
+			});
+			break;
+		case 'flipPlayerCard':
+			$clickableCards = $playerDrawDiscard.not('.player .showing');
+			$clickableCards.addClass('clickable');
+			return $clickableCards;
+			break;
+		case 'discardPile':
+			$clickableCards = $playerDrawDiscard;
+			$clickableCards.addClass('clickable');
+			return $clickableCards;
+			break;
+		case 'drawPile':
+			$clickableCards = $playerDrawDiscard.not('.draw_pile .card');
+			$clickableCards.addClass('clickable');
+			return $clickableCards;
 			break;
 	}
 }
 
-function flipPlayerCard($card) {
+function flipPlayerCard(card) {
 	gameAlert('Click the card again if you would like to flip it.');
-	$compareCard = $card;
-	$('body').on('click', '.card', function() {
-		if ($compareCard.attr('id') == $(this).attr('id')) {
+	cardIsClicked(card);
+	$clickableCards = setClickable('flipPlayerCard');
+	$clickableCards.click(function() {
+		if ($(this).hasClass('clicked')) {
 			playerNumber = $(this).parent().parent().index();
 			cardNumber = $(this).index();
-			gameAlert(gameAction('flipPlayerCard', playerNumber, cardNumber));
-			fadeError();
+			gameAction('flipPlayerCard', playerNumber, cardNumber);
 		}
 		else {
 			gameAlert('');
-			resetBinds();
+			noCardsClicked();
+			setClickable('reset');
 		}
 	});
 }
 
 function flipDrawPileCard() {
-	gameAlert('Click the draw pile again to confirm you wish to use this card.');
-	$('body').on('click', '.card', function() {
-		if ($(this).parent().parent().hasClass('draw_pile')) {
-			gameAction('flipDrawPileCard');
-		}
-		else {
-			gameAlert('');
-			resetBinds();
-		}
-	});
+	gameAction('flipDrawPileCard');
 }
 
 function takeActionWithDrawPileCard() {
 	gameAlert('Click one of your cards to swap, or discard by clicking on the discard pile.');
-	$('body').on('click', '.card', function() {
+	$('.draw_pile .card').addClass('clicked');
+	$clickable = setClickable('drawPile');
+	$clickable.click(function() {
 		if ($(this).parent().parent().hasClass('player')) {
 			playerNumber = $(this).parent().parent().index();
 			cardNumber = $(this).index();
-			gameAlert(gameAction('takeDrawPileCard', playerNumber, cardNumber));
-			$('.game_alerts').fadeTo('slow', 0.1, function() {
-				$(this).removeClass('red');
-				$(this).css('opacity', '1');
-				takeActionWithDrawPileCard();
-			});
+			gameAction('takeDrawPileCard', playerNumber, cardNumber);
 		}
 		else if ($(this).parent().parent().hasClass('discard_pile')) {
 			gameAction('discardDrawnCard');
-		}
-		else if ($(this).parent().parent().hasClass('draw_pile')) {
-			$('body').off('click', '.card');
-			takeActionWithDrawPileCard();
-		}
-		else {
-			gameAlert('');
-			resetBinds();
 		}
 	});
 }
 
 function takeDiscardPileCard() {
 	gameAlert('Click the card you would like to swap out.');
-	$('body').on('click', '.card', function() {
+	$('.discard_pile .card').addClass('clicked');
+	$clickable = setClickable('discardPile');
+	$clickable.click(function() {
 		if ($(this).parent().parent().hasClass('player')) {
 			playerNumber = $(this).parent().parent().index();
 			cardNumber = $(this).index();
-			gameAlert(gameAction('takeDiscardPileCard', playerNumber, cardNumber));
-			fadeError();
+			gameAction('takeDiscardPileCard', playerNumber, cardNumber);
 		}
 		else {
 			gameAlert('');
-			resetBinds();
+			noCardsClicked();
+			setClickable('reset');
 		}
 	});
 }
 
-function cardSwitch($this) {
-	$('body').off('click', '.card');
-	var type = $this.parent().parent().attr('class');
+function cardSwitch(clickedCard) {
+	var type = clickedCard.parent().parent().attr('class');
 	switch (type) {
 		case 'player':
-			flipPlayerCard($this);
+			flipPlayerCard(clickedCard);
 			break;
 		case 'discard_pile':
 			takeDiscardPileCard();
@@ -177,17 +175,15 @@ function cardSwitch($this) {
 
 $(document).ready(function() {
 	if (gameError != '' && gameError != null) {
-		alert(gameError);
+		gameAlert(gameError, 'red');
+		fadeError();
 	}
 	if (midAction) {
 		takeActionWithDrawPileCard();
 	}
 	else {
-		$('body').on('click', '.card', function() {
-			cardSwitch($(this));
-		});
+		setClickable('reset');
 	}
-	setClickableClass();
 });
 
 
